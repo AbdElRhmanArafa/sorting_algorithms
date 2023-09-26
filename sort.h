b7aa618d30078c36b1af98c505ea44fcdb210367
AbdElRhmanArafa <abdelrhman.arafa660@gmail.com>
2023-09-26 06:01:29 +0300

    Add quick sort

--- sort.h
+++ sort.h
@@ -21,6 +21,10 @@ void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
 void swap_nodes(listint_t **list, listint_t *first, listint_t *second);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quick_s(int *array, int lo, int hi, size_t size);
+int partition(int *array, int lo, int hi, size_t size);
+
 void bubble_sort(int *array, size_t size);
 
 #endif
